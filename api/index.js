bafa0427491f82897efa321075d03f9ae09eeb46
Ayub Kinyua <ayub@skyyalla.com>
2025-10-22 23:46:30 +0300

    chore(api): guard boot to return explicit error instead of timeout

--- api/index.js
+++ api/index.js
@@ -1,3 +1,21 @@
-const serverless = require('serverless-http');
-const app = require('../server/server');
-module.exports = serverless(app);
+let handler;
+try {
+  const serverless = require('serverless-http');
+  const app = require('../server/server');
+  handler = serverless(app);
+} catch (e) {
+  console.error('[boot] server failed to start:', e && e.message ? e.message : e);
+  handler = async (req, res) => {
+    try {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      const msg = (e && e.message) ? e.message : String(e);
+      res.end(JSON.stringify({ error: 'boot_failed', message: msg }));
+    } catch (_){
+      res.statusCode = 500;
+      res.end('boot_failed');
+    }
+  };
+}
+
+module.exports = (req, res) => handler(req, res);
