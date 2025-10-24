670340a1efa64edd9b8050adf31f359e4c748d47
Ayub Kinyua <ayub@skyyalla.com>
2025-10-24 16:21:14 +0300

    chore(config): set browser anon key to publishable key

--- public/js/app-config.js
+++ public/js/app-config.js
@@ -2,4 +2,4 @@
 // Using ecjkxgegjzvixyuukysk project (eu-north-1)
 window.SUPABASE_URL='https://ecjkxgegjzvixyuukysk.supabase.co';
 // Public anon key (safe to expose to browsers)
-window.SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjamt4Z2Vnanp2aXh5dXVreXNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5ODYzNzUsImV4cCI6MjA3NjU2MjM3NX0.4AuU78w8X7xQ_D12U9ly5WYZOPy-qR0USoe8zV8pg1o';
+window.SUPABASE_ANON_KEY='sb_publishable_iY_CGooJG4me_7X0nO0enA_OgqbIhEK';
