e6ab97f1b9c632f39a7d652fb3cd883f51048082
Ayub Kinyua <ayub@skyyalla.com>
2025-11-06 16:22:28 +0300

    Point app-config at production Supabase

--- public/js/app-config.js
+++ public/js/app-config.js
@@ -1,5 +1,3 @@
-// Supabase client configuration for the front-end
-// Using ecjkxgegjzvixyuukysk project (eu-north-1)
 window.SUPABASE_URL='https://ecjkxgegjzvixyuukysk.supabase.co';
-// Public anon key (safe to expose to browsers)
 window.SUPABASE_ANON_KEY='sb_publishable_iY_CGooJG4me_7X0nO0enA_OgqbIhEK';
+
