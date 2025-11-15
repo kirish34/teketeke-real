041300e0d344e464d1dd6c7f7a1fc06a1f667a8d
Ayub Kinyua <ayub@skyyalla.com>
2025-11-15 17:03:53 +0300

    Configure Google Maps API key

--- public/js/app-config.js
+++ public/js/app-config.js
@@ -1,3 +1,4 @@
 window.SUPABASE_URL='https://ecjkxgegjzvixyuukysk.supabase.co';
 window.SUPABASE_ANON_KEY='sb_publishable_iY_CGooJG4me_7X0nO0enA_OgqbIhEK';
+window.GMAPS_API_KEY='AIzaSyAX9n7y1QOjrOZHJwiNBkdYUJ5JET6X4aw';
 
