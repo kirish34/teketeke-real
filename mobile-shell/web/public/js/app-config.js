c3ff1c0dc3cb4cdfc4c17e092bc6045b0e4d1862
Ayub Kinyua <ayub@skyyalla.com>
2025-11-16 18:02:11 +0300

    Remove generated mobile-shell PWA assets for fresh start

--- mobile-shell/web/public/js/app-config.js
+++
@@ -1,4 +0,0 @@
-window.SUPABASE_URL='https://ecjkxgegjzvixyuukysk.supabase.co';
-window.SUPABASE_ANON_KEY='sb_publishable_iY_CGooJG4me_7X0nO0enA_OgqbIhEK';
-window.GMAPS_API_KEY='AIzaSyAX9n7y1QOjrOZHJwiNBkdYUJ5JET6X4aw';
-
--- mobile-shell/web/public/js/ui-hooks.js
+++
@@ -1,9 +0,0 @@
-window.ensureAuthOrRoot = window.ensureAuthOrRoot || (async function(){
-  try{
-    if (window.TT && window.TT.getSupabase){
-      const supa = await window.TT.getSupabase();
-      const { data:{ session } } = await supa.auth.getSession();
-      if (!session?.access_token){ location.href='/public/auth/login.html?next='+encodeURIComponent(location.pathname+location.search); }
-    }
-  }catch(_){ /* ignore */ }
-});
